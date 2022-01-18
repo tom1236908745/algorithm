ea4ea95810c49c2140ffa85157377b66d2ecaa08
tom1236908745 <tom1233908745@gmail.com>
2022-01-18 17:14:36 +0900

    234.a 234.b

--- prob/234/234b.py
+++ prob/234/234b.py
@@ -1,9 +1,13 @@
-# 未
-n = int(input())
-arr = [[0]*2]*n
-for i in range(n):
-  arr[i][0],arr[i][1] = map(int,input().split())
+import math
 
-res =
+n = int(input())
+arr = [ list(map(int,input().split(" "))) for i in range(n)]
+
+
+res = 0
 for i in range(n):
   for j in range(n):
+    temp = (arr[i][0] - arr[j][0]) ** 2 + (arr[i][1] - arr[j][1]) ** 2
+    if temp > res:
+      res = temp
+print(math.sqrt(res))
